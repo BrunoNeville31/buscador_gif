ed6137a02da6f51da9a6df96d3828c4ea86e0d44
Bruno Neville <brunoneville31@gmail.com>
2020-04-10 22:02:20 -0300

    Aplicação desenvolvida em Flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:buscador_gif/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscador_gif/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Buscador de Gif",
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white70),
+  ));
+}
+
+
